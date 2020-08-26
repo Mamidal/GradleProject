6e796b55934787ffa908b17d00c3d0be0da96441
Jayavardhan Chowki <39634557+jayavardhan123@users.noreply.github.com>
2020-08-25 21:21:37 -0400

    Update LibraryTest.java

--- src/test/java/SonarTest/LibraryTest.java
+++ src/test/java/SonarTest/LibraryTest.java
@@ -9,7 +9,6 @@ import static org.junit.Assert.*;
 public class LibraryTest {
     @Test 
     public void testSomeLibraryMethod() {
-        Library classUnderTest = new Library();
-        assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
+        System.out.println("My Test");
     }
 }

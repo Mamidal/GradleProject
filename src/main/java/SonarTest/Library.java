81988d3dfebae073db393f48924be932fcca69f9
Jayavardhan Chowki <39634557+jayavardhan123@users.noreply.github.com>
2020-08-25 21:21:00 -0400

    Update Library.java

--- src/main/java/SonarTest/Library.java
+++ src/main/java/SonarTest/Library.java
@@ -3,8 +3,34 @@
  */
 package SonarTest;
 
+import java.util.ArrayList;
+
 public class Library {
-    public boolean someLibraryMethod() {
-        return true;
+	public void m1() {
+    	String s = "";
+    	ArrayList<Integer> list = new ArrayList<Integer>();
+    	list.add(1);
+    	list = null;
+    	list.add(2);
+    	Object obj = getData();
+    	System.out.println(obj.toString());
+    			
+    }    
+     
+    public Object getData() {
+    	return null;
     }
+	
+	public void addition(int a , int b) {
+		int res = a + b;
+	}
+    
+	public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        Library app = new Library();
+        app.m1();
+    }
+    
 }
+
